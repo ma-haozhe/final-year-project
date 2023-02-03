0a26dfbab38f5c1a2d72051d8d89a5283cc901e1
ma-haozhe <mahaozhe2012@gmail.com>
2023-02-03 02:24:30 +0000

    uploading a lot of images

--- gpu.py
+++ gpu.py
@@ -17,4 +17,4 @@ print(f"Pandas {pd.__version__}")
 #print(f"Scikit-Learn {sk.__version__}")
 #print(f"SciPy {sp.__version__}")
 gpu = len(tf.config.list_physical_devices('GPU'))>0
-print("GPU is", "available" if gpu else "NOT AVAILABLE")
+print("GPU is", "available" if gpu else "NOT AVAILABLE")    
