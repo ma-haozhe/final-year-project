efe6c07bda5604258d69e47ccff87399f4382fa8
ma-haozhe <mahaozhe2012@gmail.com>
2023-03-06 19:40:28 +0000

    adding the convert func from json to obj locator training file

---
+++ parse_json_to_obj_locator_train.py
@@ -0,0 +1,49 @@
+#this script parse annotated json file from label studio
+#to training file for object locator from 2019 paper
+# object localization without bounding boxes, to test 
+#the effectiveness of their work on Archery targets.
+# Haozhe Ma
+# March 2023
+
+import json
+import csv
+
+# open the file in the write mode
+f = open('/Users/haozhema/Documents/Final year project/gt.csv', 'w')
+
+# create the csv writer
+writer = csv.writer(f)
+
+
+# load the JSON data from the file
+anno_file = "project-1-at-2023-03-06-17-55-c451917c.json"
+img_file_dir = "60cm/"
+with open(anno_file, 'r') as f:
+    data = json.load(f)
+
+for obj in data:
+    # extract the image file name
+    img_file = obj['data']['img']
+    img_file = img_file_dir + img_file.split("-")[1]
+    #print(img_file)
+
+    # extract the x and y values for each dot and calculate the plot coordinates
+    dots = []
+    for annotation in obj['annotations']:
+        for result in annotation['result']:
+            x = result['value']['x'] * result['original_width']/100
+            y = result['value']['y'] * result['original_height']/100
+            dots.append((x, y))
+            print(x,', ',y)
+    #这里坐标点float可能会出问题
+    #print(dots)
+    row = [img_file,len(dots),dots]
+    print(row)
+    # write a row to the csv file
+    writer.writerow(row)
+
+
+# close the file
+f.close()
+
+    
