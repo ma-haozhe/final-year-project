8f94473e765d00c3a510fee7f87fcd8b82b283a0
ma-haozhe <mahaozhe2012@gmail.com>
2023-03-22 13:00:35 +0000

    updating filename

--- mask_and_morph_close.py
+++ mask_and_morph_close.py
@@ -1,10 +1,24 @@
 import numpy as np
 import cv2
 
+# This code loads an image and converts it to the HSV color space.
+# Then, it sets a range of colors to detect (in this case, yellow)
+# and creates a binary mask that highlights the parts of the image that fall within that color range.
+# The code then finds the contours in the mask and filters them based on the number of vertices.
+# Finally, it draws the contours that meet the criteria on the original image and displays both
+# the mask and the original image with the contours drawn.
+# The resulting images are saved as "mask.png" and "original.png".
+
 # Load the input image
 image = cv2.imread('60cm/272.jpeg')
 original = image.copy()
 image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+
+# blue lower[100,50,50] upper[130,255,255]
+# yellow lower[20,100,100] upper[40,255,255]
+# red lower[0,50,50] upper[10,255,255]
+# red is good.
 
 # Define the color ranges for red and yellow
 lower_red = np.array([0, 100, 100], dtype="uint8")
@@ -26,18 +40,20 @@ cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 print('Number of contours found: {}'.format(len(cnts)))
 
 # Create a kernel for morphological closing
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (25, 25))
 # Perform morphological closing on the contours
 closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
 # Find contours in the closed image
-cnts_closing = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts_closing = cv2.findContours(
+    closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 # Extract contours depending on OpenCV version
 cnts_closing = cnts_closing[0] if len(cnts_closing) == 2 else cnts_closing[1]
 print('Number of contours found after closing: {}'.format(len(cnts_closing)))
 
 contour_count = 0
 contour_count_closing = 0
-# Iterate through contours and filter by the number of vertices 
+# Iterate through contours and filter by the number of vertices
 for c in cnts:
     perimeter = cv2.arcLength(c, False)
     approx = cv2.approxPolyDP(c, 0.04 * perimeter, False)
